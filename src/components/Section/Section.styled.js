d923554b34e9699b42ee40115fecb02fc8d816cd
OttoCarius <ottocarius1993@gmail.com>
2022-09-30 15:28:11 +0300

    fx

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledSection = styled.section`
-  /* padding-bottom: ${p => p.theme.space[4]}px; */
+  padding-bottom: ${p => p.theme.space[4]}px;
 
   flex-direction: column;
   align-items: center;
@@ -10,7 +10,7 @@ export const StyledSection = styled.section`
 export const StyledTitle = styled.h1`
   /* margin-bottom: 10px; */
   color: ${p => p.theme.colors.primaryText};
-  background-color: ${p => p.theme.colors.white};
+  /* background-color: ${p => p.theme.colors.white}; */
   text-shadow: 0 1px #808d93, -1px 0 #cdd2d5, -1px 2px #808d93, -2px 1px #cdd2d5,
     -2px 3px #808d93, -3px 2px #cdd2d5, -3px 4px #808d93, -4px 3px #cdd2d5,
     -4px 5px #808d93, -5px 4px #cdd2d5, -5px 6px #808d93, -6px 5px #cdd2d5,
